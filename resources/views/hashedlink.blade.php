a74a748f1bd88e11ffdb143c88a34bcf7098663a
Vinayak Sawant <vinayaksawant318@gmail.com>
2020-06-26 22:11:54 +0530

    Warning for duplicate link added, Random code check added

--- resources/views/hashedlink.blade.php
+++ resources/views/hashedlink.blade.php
@@ -41,6 +41,12 @@
                     {{ Session::get('success') }}
                 </div>
             @endif
+
+            @if (Session::has('warning'))
+                <div class="alert alert-warning" role="alert">
+                    {{ Session::get('warning') }}
+                </div>
+            @endif
    
             <table id="hashLinksTable" class="table table-bordered table-sm">
                 <thead>
